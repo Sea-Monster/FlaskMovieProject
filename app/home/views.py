d759f4672f7a6f354fe3ed07fd7356aa3bf45588
Sea-Monster <cristianofigo@126.com>
2017-12-06 09:47:05 +0800

    会员登录页面搭建

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 from . import home
-from flask import render_template
+from flask import render_template, redirect, url_for
+
 
 # 调用蓝图
 @home.route('/')
 def index():
     return render_template('home/index.html')
+
+
+@home.route('/login/')
+def login():
+    return render_template('home/login.html')
+
+
+@home.route('/logout/')
+def logout():
+    return redirect(url_for('home.login'))
