338e02cd30fef1bf088d249d10fd52bb86d90693
Sea-Monster <cristianofigo@126.com>
2017-12-06 17:49:53 +0800

    404页面

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 app.debug=True
 
@@ -9,3 +9,8 @@ from app.admin import admin as admin_blueprint
 # 注册蓝图
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
+# 定义404页面
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('home/404.html'), 404
