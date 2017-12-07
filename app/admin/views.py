98bddf9816786ae157518aad3ea955ec154a14b6
Sea-Monster <cristianofigo@126.com>
2017-12-07 14:54:42 +0800

    会员列表，会员详情

--- app/admin/views.py
+++ app/admin/views.py
@@ -62,3 +62,16 @@ def preview_add():
 @admin.route('/preview/list/')
 def preview_list():
     return render_template('admin/preview_list.html')
+
+
+# -会员管理页面 -
+# 会员列表
+@admin.route('/user/list/')
+def user_list():
+    return render_template('admin/user_list.html')
+
+
+# 查看会员
+@admin.route('/user/view/')
+def user_view():
+    return render_template('admin/user_view.html')
