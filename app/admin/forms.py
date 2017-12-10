ae044071153321f8026809fc516704d89c0b9d46
Sea-Monster <cristianofigo@126.com>
2017-12-10 16:44:40 +0800

    标签管理

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -2,7 +2,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
-from app.models import Admin
+from app.models import Admin, Tag
 
 
 class LoginForm(FlaskForm):
@@ -54,3 +54,25 @@ class LoginForm(FlaskForm):
         admin = Admin.query.filter_by(name=account).count()
         if admin == 0:
             raise ValidationError('账号不存在！')
+
+
+class TagForm(FlaskForm):
+    name = StringField(
+        label='名称',
+        validators=[
+            DataRequired('请输入标签！')
+        ],
+        description='标签',
+        render_kw={
+            'class':'form-control',
+            'id':'input_name',
+            'placeholder':'请输入标签名称！'
+        },
+    )
+
+    submit = SubmitField(
+        '添加',
+        render_kw={
+            'class': 'btn btn-primary'
+        }
+    )
